55ef1fb3c33d18a249e14f649a99a9016ae0d31f
Tanmay Karad <154725109+karad-tanmay@users.noreply.github.com>
2024-10-08 19:40:38 +0530

    commented include files in string_array_conversion.c to avoid warnings of redifinition

--- Proto_1_txt_file/string_tokenization/string_array_conversion.c
+++ Proto_1_txt_file/string_tokenization/string_array_conversion.c
@@ -25,16 +25,16 @@
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/file.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <errno.h>
-#include "../List_ADT/linkedlist.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <fcntl.h>
+// #include <sys/types.h>
+// #include <sys/file.h>
+// #include <sys/stat.h>
+// #include <unistd.h>
+// #include <errno.h>
+// #include "../List_ADT/linkedlist.h"
 
 #define MAX_SIZE 1024
 
